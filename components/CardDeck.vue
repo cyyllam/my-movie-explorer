25976574c59894c412ec5d0c1ee76e7ec5f6c4e2
Christy Lam <cyyllam@gmail.com>
2020-06-07 11:23:37 -0700

    added fallback photo and alt text if missing

--- components/CardDeck.vue
+++ components/CardDeck.vue
@@ -5,8 +5,8 @@
     style="max-width: 13rem;" 
     class="mb-3" 
     :title="result.title" 
-    v-bind:img-src="'http://image.tmdb.org/t/p/w500/' + result.poster_path" 
-    v-bind:img-alt="'Poster image of ' + result.title + ' from tMDB'" 
+    v-bind:img-src="renderImg(result.poster_path)" 
+    v-bind:img-alt="renderImgAlt(result.poster_path, result.title) "
     img-top>
         <b-card-text>
           <p>User rating: {{ result.vote_average }}</p> 
@@ -21,13 +21,44 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // fallback: 'https://images.unsplash.com/photo-1516541196182-6bdb0516ed27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+      fallback: {
+        image:'https://images.unsplash.com/photo-1539114283047-162d7e532da4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+        alt: 'Image by Fabrizio Conti from Unsplash https://unsplash.com/@staticlaw'
+      }
+    }
+
+  },
 	props: {
     // result: Object
     result: {
       type: Object,
       required: true
     }
+    },
+  methods: {
+    renderImg(anImg) {
+      let finalImg = null;
+      if (anImg === null) {
+        finalImg = this.fallback.image;
+      } else {
+        finalImg = 'http://image.tmdb.org/t/p/w500/' + anImg;
+      }
+      return finalImg;
+    },
+    renderImgAlt(anImg, aTitle) {
+      let finalStr = null;
+      if (anImg === null) {
+        finalStr = this.fallback.alt;
+      } else {
+        finalStr = 'Poster image of ' + aTitle + ' from tMDB';
+      }
+      return finalStr;
+
     }
+  }
 }
 </script>
 
